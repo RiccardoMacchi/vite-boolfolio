f5f01cc14ee8cabdf811adf1aba8ce9260931b0a
RiccardoMacchi <rick.mac97@outlook.com>
2024-10-01 15:38:54 +0200

    added axios call and printed on page list

--- src/pages/Items.vue
+++ src/pages/Items.vue
@@ -1,11 +1,38 @@
 <script>
+import axios from 'axios';
+import { store } from '@/store/store';
+
 export default{
-    name: 'items'
+    name: 'items',
+    data(){
+        return{
+            items: [],
+        };
+    },
+
+    methods:{
+        getApi(){
+            axios.get(store.urlApi + 'items')
+                .then(response =>{
+                    this.items = response.data.data.data
+                    console.log(this.items)
+                })
+            
+        }
+    },
+    mounted(){
+        this.getApi();
+    }
 }
 </script>
 
 <template>
     <h1>ITEMS</h1>
+    <div>
+        <ul>
+            <li v-for="item in items"> {{ item.title }}</li>
+        </ul>
+    </div>
 </template>
 
 <style lang="scss" scoped>
