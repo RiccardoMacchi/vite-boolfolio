2581b73ddb6a6a44548323dfb06576598316eefd
RiccardoMacchi <rick.mac97@outlook.com>
2024-10-01 15:28:48 +0200

    added home and items page with routing

---
+++ src/pages/Items.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name: 'items'
+}
+</script>
+
+<template>
+    <h1>ITEMS</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/partials/Navbar.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    name: 'navbar'
+}
+
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link :to="{name:'home'}">Home</router-link>
+            </li>
+            <li>
+                <router-link :to="{name:'items'}">Lavori</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
