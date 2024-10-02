f55b4ab1091c7b1faf3098826f5ad96a04d098bd
RiccardoMacchi <rick.mac97@outlook.com>
2024-10-02 17:19:07 +0200

    added 404 page

---
+++ src/pages/Error404.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+    name: '404'
+}
+
+</script>
+
+<template>
+    <h1>ERROR 404</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import Items from '@/pages/Items.vue'
 import ItemDetails from '@/pages/ItemDetails.vue'
+import Error404 from '@/pages/Error404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/dettagli-lavoro/:slug',
       name: 'itemsDetails',
       component: ItemDetails
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: '404',
+      component: Error404
     },
   ]
 })
