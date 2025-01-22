4b7085f30934e8a57522155a6254e888cecf9af3
Riccardo Macchi <rick.mac97@outlook.com>
2025-01-22 17:42:40 +0100

    faet(items): little layout improve

--- src/pages/Items.vue
+++ src/pages/Items.vue
@@ -73,10 +73,18 @@ export default{
 </script>
 
 <template>
-    <h2>ITEMS</h2>
-    <div class="search_bar">
-        <i class="fa-solid fa-magnifying-glass"></i>
-        <input type="search" v-model="search">
+    <div>
+        <h2>I miei Lavori</h2>
+        <div class="wrap-img">
+            <img src="../../public/my_works.svg" alt="">
+        </div>
+    </div>
+    <div class="wrapp-search">
+        <h4>Carca tra tutti i lavori:</h4>
+        <div class="search_bar">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="search" v-model="search">
+        </div>
     </div>
     <div class="wrapper">
         <div class="wrapper_item">
@@ -86,40 +94,51 @@ export default{
                 <div v-else-if="!items.length">
                     <h3>NON CI SONO LAVORI</h3>
                 </div>
-                <ul v-else>
-                    <router-link v-for="item in items" :to="{name:'itemsDetails', params:{'slug' : item.slug}}">
-                    <li>
-                        <h4>{{ item.title }}</h4>
-                        {{console.log(item)}}
-                        <!-- Small descrizonale -->
-                        <small>
-                            <h6>Teconologie usate:</h6>
-                            <span v-if="item.technologies.length === 0">
-                                <a class="badge badge-tech">NESSUNA TECNOLOGIA</a>
-                            </span>
-                            <span v-else v-for="tech in item.technologies">
-                                <router-link class="badge badge-tech" :to="{name:'lavoriByTech', params:{'slug' : tech.slug}}">{{ tech.name }}</router-link>
-                            </span>
-                        </small>
-                        <small>
-                            <h6>Tipologia:</h6>
-                            <span>
-                                <router-link class="badge badge-type" :to="{name:'lavoriByType', params:{'slug': item.type.slug}}">{{ item.type.name }}</router-link>
-                            </span>
-                        </small>
-                        <small>
-                            <h6>Freamworks usati:</h6>
-                            <span v-if="item.frameworks.length === 0">
-                                <a class="badge badge-framework">NESSUN FRAMEWORK</a>
-                            </span>
-                            <span v-else v-for="framework in item.frameworks">
-                                <router-link class="badge badge-type" :to="{name:'lavoriByType', params:{'slug': framework.slug}}">{{ framework.name }}</router-link>
-                            </span>
-                        </small>
-                        <img class="thumb" :src="baseUrl + item.img_path" :alt="item.title">
-                    </li>
-                    </router-link>
-                </ul>
+                <div v-else>
+                    <span class="right">Lavori disponibili: {{ items.length }}</span>
+                    <div class="wrapper-cards">
+                        <router-link v-for="item in items" :to="{name:'itemsDetails', params:{'slug' : item.slug}}" class="card">
+                            <div class="div-card">
+                                <h4>{{ item.title }}</h4>
+                                {{console.log(item)}}
+                                <div class="thumb">
+                                    <img :src="baseUrl + item.img_path" :alt="item.title">
+                                </div>
+                                <div class="description">
+                                    <h5>Una breve descrizione:</h5>
+                                    <p>{{ item.description }}</p>
+                                </div>
+                                <!-- div descrizonale -->
+                                <div class="wrap-item-tech-type">
+                                    <div>
+                                        <i class="fa-solid fa-code"></i>
+                                        <span v-if="item.technologies.length === 0">
+                                            <a class="badge badge-tech">NESSUNA TECNOLOGIA</a>
+                                        </span>
+                                        <span v-else v-for="tech in item.technologies">
+                                            <router-link class="badge badge-tech" :to="{name:'lavoriByTech', params:{'slug' : tech.slug}}">{{ tech.name }}</router-link>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <i class="fa-solid fa-server"></i>
+                                        <span>
+                                            <router-link class="badge badge-type" :to="{name:'lavoriByType', params:{'slug': item.type.slug}}">{{ item.type.name }}</router-link>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <i class="fa-solid fa-briefcase"></i>
+                                        <span v-if="item.frameworks.length === 0">
+                                            <a class="badge badge-framework">NESSUN FRAMEWORK</a>
+                                        </span>
+                                        <span v-else v-for="framework in item.frameworks">
+                                            <router-link class="badge badge-type" :to="{name:'lavoriByFramework', params:{'slug': framework.slug}}">{{ framework.name }}</router-link>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </router-link>
+                    </div>
+                </div>
                 <span>{{ console.log(paginatorLink) }}</span>
             <div v-if="paginatorLink.length > 3" class="paginator_btn">
                 <button v-for="link in paginatorLink" v-html="link.label" @click="getApi(link.url)" :disabled="link.active || !link.url"></button>
@@ -163,29 +182,87 @@ export default{
 
 <style lang="scss" scoped>
 
-ul{
-    list-style: none;
-    li{
-        position: relative;
-        padding-left: 10px;
-        line-height: 25px;
-        border-radius: 10px;
-        border: 1px solid grey;
-        margin: 5px;
-        &:hover{
-            background-color: rgba(82, 82, 82, 0.7);
-        }
-        .thumb{
-            max-width: 100px;
-            border-radius: 5px;
-            position: absolute;
-            aspect-ratio: 1/1;
-            object-fit: cover;
-            right: 20px;
-            top: 50%;
-            transform: translate(0,-50%)
-        }
+h2{
+    font-size: 4rem;
+    text-align: center;
+    margin: 20px auto;
+}
+.wrap-img{
+    width: 70%;
+    margin: 30px auto;
+    img{
+        width: 100%;
     }
+}
+
+span{
+    
+    &.right{
+        text-align: right;
+        margin-right: 20px;
+        display: block;
+    }
+}
+
+.wrapp-search{
+    h4{
+        font-size: 1.7rem;
+    }
+}
+
+// Cards
+
+.wrapper-cards{
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 30px;
+        .card{
+            flex: 0 0 calc(100% / 2 - 30px);
+            .div-card{
+                h4{
+                    font-size: 2.2rem;
+                    margin-bottom: 20px;
+                }
+                .thumb{
+                    width: 60%;
+                    margin: 0 auto;
+                    img{
+                        border-radius: 5px;
+                        aspect-ratio: 1/1;
+                        object-fit: cover;
+                        width: 100%;
+                    }
+                }
+
+                .description{
+                    h5{
+                        font-size: 1.2rem;
+                    }
+                    p{
+                        margin: 10px auto;
+                    }
+                }
+
+                .wrap-item-tech-type{
+                    margin-top: 30px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-wrap: wrap;
+                    gap: 20px;
+                    div{
+                        display: flex;
+                        align-items: center;
+                    }
+                    .badge{
+                        padding: 8px;
+                        border-radius: 10px;
+                    }
+                }
+            }
+        }
+    
 }
 
 .wrapper{
@@ -196,18 +273,18 @@ ul{
         display: flex;
         flex-direction: column;
 
-        small{
-            display: block;
-            padding-left: 15px;
-            h6{
-                display: inline-block;
-                margin-right: 4px
-            }
-            span{
-                a{
-                    width: auto;
-                    margin-right: 8px;
-                }
+        i{
+            color: rgb(219, 240, 177);
+            padding: 5px;
+            background: #3d3d3d;
+            border-radius: 5px;
+            margin-right: 5px;
+        }
+
+        span{
+            a{
+                width: auto;
+                margin-right: 8px;
             }
         }
         
@@ -225,6 +302,9 @@ ul{
 
     .wrapper_cath{
         margin-left: 15px;
+        h4{
+            font-size: 1.2rem;
+        }
         div{
             margin-bottom: 15px;
             max-width: 100px;
